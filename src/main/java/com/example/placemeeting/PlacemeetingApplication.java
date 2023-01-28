e6713549e6add6d58cb39480fa0aa2d6b2c6ef1e
leeseungwoo <vugil@naver.com>
2023-01-28 19:59:44 +0900

    feat : 채팅기능 구현

---
+++ src/main/java/com/example/placemeeting/PlacemeetingApplication.java
@@ -0,0 +1,13 @@
+package com.example.placemeeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacemeetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlacemeetingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/placemeeting/PlacemeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.placemeeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacemeetingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
