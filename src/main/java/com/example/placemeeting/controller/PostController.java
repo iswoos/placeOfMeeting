fc18f1003c48898154c8e6a2c2d6e66782563c8b
leeseungwoo <vugil@naver.com>
2023-02-19 23:56:09 +0900

    Comment : Post 관련 service 개발중

--- src/main/java/com/example/placemeeting/controller/PostController.java
+++ src/main/java/com/example/placemeeting/controller/PostController.java
@@ -1,16 +1,13 @@
 package com.example.placemeeting.controller;
 
-import com.example.placemeeting.dto.reqeustdto.MemberRequest;
-import com.example.placemeeting.dto.responsedto.MemberResDto;
 import com.example.placemeeting.global.dto.ResponseDto;
+import com.example.placemeeting.security.user.UserDetailsImpl;
 import com.example.placemeeting.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -20,10 +17,9 @@ public class PostController {
     private final PostService postService;
 
 
-
     @GetMapping("/posts")
-    public ResponseDto<List> allPosts()
-
+    public ResponseDto<List> getPosts(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return ResponseDto.success(postService.getPosts(userDetails.getAccount()));
     }
 
 }
--- src/main/java/com/example/placemeeting/service/PostService.java
+++ src/main/java/com/example/placemeeting/service/PostService.java
@@ -1,4 +1,11 @@
 package com.example.placemeeting.service;
 
+import com.example.placemeeting.domain.Member;
+
 public class PostService {
+    public Object getPosts(Member account) {
+
+
+
+    }
 }
