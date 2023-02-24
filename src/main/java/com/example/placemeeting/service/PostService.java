aaf8fbfca7015aa438a75ab87498200c154d9d75
leeseungwoo <vugil@naver.com>
2023-02-24 16:43:36 +0900

    Refactor : stream 활용하여 기존 코드 리팩토링

--- src/main/java/com/example/placemeeting/service/PostService.java
+++ src/main/java/com/example/placemeeting/service/PostService.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -33,18 +34,23 @@ public class PostService {
 
         List<Post> postList = postRepository.findByPostTypeAndCityName(PostType.valueOf(postType), member.getCityName());
 
-        // 생성일자 기준 최신순으로 정렬
-        postList.sort((p1,p2) -> p2.getCreatedAt().compareTo(p1.getCreatedAt()));
+        return postList.stream() // postList를 stream으로 변환
+                .sorted(Comparator.comparing(Post::getCreatedAt).reversed()) // 생성일자 최신기준으로 정렬
+                .map(PostMainResDto::new)
+                /*메서드 반환타입을 맞추기 위해 map을 이용. stream의 map() 메서드는 입력 스트림의 각 요소를 다른 요소로 매핑하여,
+                매핑된 요소들을 새로운 스트림으로 변환하는 용도로 편하게 사용됨*/
 
-        return postList.stream().map(PostMainResDto::new).collect(Collectors.toList());
+                //PostMainResDto::new는 클래스의 생성자를 참조하여 새로운 PostMainResDto 객체 생성함
+                //map메서드에서는 각각의 객체를 받아 stream 요소에서 PostMainResDto 객체로 매핑된 요소들을 새로운 stream으로 반환한다.
 
+                .collect(Collectors.toList());
+                /*collect 메서드는 스트림의 최종 연산 중 하나로, 스트림의 요소들을 모아서 특정한 컬렉션 타입으로 반환해준다.
+                매개변수는 Collector를 받으며, Collector는 스트림 요소를 수집하는 역할을 한다. Collector를 이용하여 스트림의 요소들을 모아서 List, Set, Map등의 구조로 변환할 수 있다.*/
 
-//        List<PostMainResDto> responseDto = new ArrayList<>();
-
-//        for (Post post : postList) {
-//            responseDto.add(new PostMainResDto(post));
-//        }
-//        return responseDto;
+                /*Collectors 클래스는 Collector 객체를 생성하기 위한 static 메소드를 제공한다. 활용예시는 하단에 기재.
+                Collectors.toList()는 스트림 요소를 List로 수집하는 Collector를 생성
+                Collectors.toSet()은 스트림 요소를 Set으로 수집하는 Collector를 생성
+                Collectors.toMap()은 스트림 요소를 Map으로 수집하는 Collector를 생성*/
     }
 
     @Transactional
