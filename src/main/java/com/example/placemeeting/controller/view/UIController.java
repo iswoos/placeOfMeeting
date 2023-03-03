09caa2b5b975e14d2e34a524ce8679f58e2353bc
leeseungwoo <vugil@naver.com>
2023-03-03 21:43:44 +0900

    Refactor : 로그인페이지 템플릿 @GetMapping 컨트롤러 주소변경

--- src/main/java/com/example/placemeeting/controller/view/UIController.java
+++ src/main/java/com/example/placemeeting/controller/view/UIController.java
@@ -13,7 +13,7 @@ public class UIController {
     // 해당 컨트롤러를 통해 페이지를 출력한 것을 조금 더 다른 방식으로 개선할 수 있지않겠는가? 라는 피드백을 주셨음.
     // 이 출력과정을 어떻게 바꿔볼지 공부가 필요함...!
     // 모든 확장자 조회
-    @GetMapping("/members/login")
+    @GetMapping("/")
     public String accountsLoginForm() {
         return "accounts/accountLoginForm";
     }
